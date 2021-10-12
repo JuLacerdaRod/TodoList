4c9c9e20e88c17b0f5e723fbd8a0b71de4033406
Ju Lacerda <julacerdarod@gmail.com>
2021-10-12 19:44:17 +1000

    first commit

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+function TodoForm(props) {
+    const { addTodo } = props;
+    const [value, setValue] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        event.preventDefault();
+        if (!value) {
+        return;
+        }
+        addTodo(value);
+        setValue("");
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+        <input 
+        value={value}
+        type="text" 
+        placeholder="Add To do..." 
+        onChange={(event) => setValue(event.target.value)}
+        />
+        </form>
+    );
+}
+
+export default TodoForm;
